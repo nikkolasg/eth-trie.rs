35c94a4aacb7570cc216290d4046412f8f91f1ad
nikkolasg <nikkolasg@gmail.com>
2024-01-31 13:02:08 +0100

    removed duplicates

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,6 @@ pub use errors::{MemDBError, TrieError};
 pub use nibbles::Nibbles;
 pub use node::Node;
 pub use trie::{EthTrie, Trie};
-pub use node::Node;
-pub use nibbles::Nibbles;
 
 #[doc = include_str!("../README.md")]
 #[cfg(doctest)]
